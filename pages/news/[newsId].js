ab89ece62d7e5522dfecee0a021fbce9e7f88d81
rl627 <rl627@drexel.edu>
2021-10-05 14:35:05 -0400

    added useRouter() to dynamic newsId pages

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,14 @@
+import { useRouter } from 'next/router';
+
 // our-domain.com/[newsId]
 
 function DetailPage() {
+  const router = useRouter();
+  const newsId = router.query.newsId;
+  console.log(newsId);
+
+  // send a request to a backend API to fetch news item with newsId
+
   return <h1>The Detail Page</h1>;
 }
 
