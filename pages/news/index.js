bd6facf3d25c9e346493bd4faba63fc8c1af55e6
rl627 <rl627@drexel.edu>
2021-10-05 15:17:16 -0400

    news links with anchor tags

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,25 @@
+import { Fragment } from 'react';
+
 // our-domain.com/news
 
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <a href="/news/nextjs-is-a-great-framework">
+            NextJS Is a Great Framework
+          </a>
+        </li>
+        <li>
+          <a href="/news/something-else">
+            Something Else
+          </a>
+        </li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
